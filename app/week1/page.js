7d0006b24dfc170e8caec01f83892500aa1a32f4
1239874569898 <Qianying.Ruan@edu.sait.ca>
2023-11-16 15:10:38 -0800

    Adding all weeks

---
+++ app/week1/page.js
@@ -0,0 +1,10 @@
+export default function Week1() {
+    return (
+      <main>
+        <h1>Week 1 Assignment</h1>
+        <p>
+          Github account:Qianying Ruan (1239874569898)
+        </p>
+      </main>
+    );
+  }
